9d161ac1d12105137649cfe7b3f513859883bc74
Kendall Morgan <kuyukip@gmail.com>
2022-07-19 19:39:38 -0500

    create comments model, make migrations, adjust urlpatterns, create serializer, add get and post to views

--- backend/comments/models.py
+++ backend/comments/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from authentication.models import User
 
 # Create your models here.
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    video_id = models.CharField(max_length=50)
+    text = models.CharField(max_length=200)
+    likes = models.IntegerField()
+    dislikes = models.IntegerField()
