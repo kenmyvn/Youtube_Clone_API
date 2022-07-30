c0f619a136afd649a77f761b1683a1bc0daaaaba
Kendall Morgan <kuyukip@gmail.com>
2022-07-30 15:36:37 -0500

    get data to appear on home page

--- frontend/src/pages/HomePage/HomePage.js
+++ frontend/src/pages/HomePage/HomePage.js
@@ -1,29 +1,32 @@
-import React from "react";
-import { useEffect, useState } from "react";
+import React, { useState, useEffect } from "react";
 import {KEY} from "../../localKey";
 import axios from "axios";
 import { DATA } from "../../localData";
 
 const HomePage = () => {
-  const [videos, setVideos] = useState([DATA]);
+  const [videos, setVideos] = useState(DATA.items);
 
   useEffect(() => {
-    const fetchVideos = async () => {
-      try {
-        let response = await axios.get(`https://www.googleapis.com/youtube/v3/search?q=monstax&key=${KEY}&part=snippet&type=video&maxResults=5`);
-        setVideos(response.data);
-      } catch (error) {
-        console.log(error.message);
-      }
-    };
-    fetchVideos();
-  }, );
+    // fetchVideos();
+  }, []);
+
+  const fetchVideos = async () => {
+    try {
+      let response = await axios.get(`https://www.googleapis.com/youtube/v3/search?q=monstax&key=${KEY}&part=snippet&type=video&maxResults=5`);
+      setVideos(response.data);
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
 
   return (
     <div className="container">
       {videos &&
         videos.map((video) => {
-          return <li key={video.id}>{video.name}</li>;
+          return <li key={video.snippet.title}>
+            {video.snippet.title}
+            <img src={video.snippet.thumbnails.default.url} />
+          </li>;
         })}
     </div>
   );
