4620d3ab18d254fbce2847371f1ce0489f77e67b
Kendall Morgan <kuyukip@gmail.com>
2022-08-02 18:45:38 -0500

    adjust search page

--- frontend/src/pages/SearchResultsPage/SearchResultsPage.js
+++ frontend/src/pages/SearchResultsPage/SearchResultsPage.js
@@ -1,19 +1,21 @@
 import React, { useState, useEffect } from "react";
-import {KEY} from "../../localKey";
+import { KEY } from "../../localKey";
 import axios from "axios";
 import { DATA } from "../../localData";
 import "./SearchResultsPage.css";
 
 const SearchResultsPage = () => {
-  const [videos, setVideos] = useState(DATA.items);
+  const [videos, setVideos] = useState();
 
   useEffect(() => {
-    // fetchVideos();
+    fetchVideos();
   }, []);
 
   const fetchVideos = async () => {
     try {
-      let response = await axios.get(`https://www.googleapis.com/youtube/v3/search?q=monstax&key=${KEY}&part=snippet&type=video&maxResults=5`);
+      let response = await axios.get(
+        `https://www.googleapis.com/youtube/v3/search`
+      );
       setVideos(response.data);
     } catch (error) {
       console.log(error.message);
@@ -24,10 +26,12 @@ const SearchResultsPage = () => {
     <div className="videoscontainer">
       {videos &&
         videos.map((video) => {
-          return <div className="vid" key={video.snippet.title}>
-            <p>{video.snippet.title}</p>
-            <img src={video.snippet.thumbnails.medium.url} />
-          </div>;
+          return (
+            <div className="vid" key={video.snippet.title}>
+              <p>{video.snippet.title}</p>
+              <img src={video.snippet.thumbnails.medium.url} />
+            </div>
+          );
         })}
     </div>
   );
